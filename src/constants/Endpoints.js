aec7bc472116ea7ff181a95a42794162cc312b87
Niclas <66620423+Niclas-Kedzierski@users.noreply.github.com>
2022-12-18 21:46:08 +0100

    Create Endpoints.js

---
+++ src/constants/Endpoints.js
@@ -0,0 +1,6 @@
+class Endpoints {
+    static CFX_MASTERLIST = 'https://servers-frontend.fivem.net/api/servers/single/';
+    static CFX_STATUS_SUMMARY = 'https://status.cfx.re/api/v2/status.json';
+}
+  
+module.exports = Endpoints;
